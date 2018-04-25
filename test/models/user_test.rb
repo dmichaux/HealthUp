49afb015ef0b735b5f82c0eecd9a182dcdf451e6
David Michaux <michaux.dr@gmail.com>
2018-04-25 12:34:53 -0400

    Add basic tests for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = User.new(name: "Example User", email: "user@example.com")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "should not be admin" do
+		assert_not @user.admin?
+	end
 end
