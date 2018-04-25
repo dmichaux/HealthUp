5d0116455ed989fd68175180e8dd18510d9cdb59
David Michaux <michaux.dr@gmail.com>
2018-04-25 13:34:06 -0400

    Test for and add validations for User name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+
+	before_save :downcase_email
+
+	validates :name,  presence: true, length: { within: 3..25 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: { within: 5..254 },
+										format: { with: VALID_EMAIL_REGEX },
+										uniqueness: { case_sensitive: false}
+
+	private
+
+	def downcase_email
+		self.email.downcase!
+	end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,60 @@ class UserTest < ActiveSupport::TestCase
 	test "should not be admin" do
 		assert_not @user.admin?
 	end
+
+	test "name should be present" do
+		@user.name = "   "
+		assert_not @user.valid?
+	end
+
+	test "name should be 3 - 25 characters long" do
+		@user.name = "XX"
+		assert_not @user.valid?
+		@user.name = ("X" * 26)
+		assert_not @user.valid?
+	end
+
+	test "email should be present" do
+		@user.email = "   "
+		assert_not @user.valid?
+	end
+
+	test "email should be 5 - 254 characters long" do
+		@user.email = "XXXX"
+		assert_not @user.valid?
+		@user.email = ("X" * 255)
+		assert_not @user.valid?
+	end
+
+	test "email should be saved downcase" do
+		mixed_case_email = "Foo@BaR.cOm"
+		@user.email = mixed_case_email
+		@user.save
+		assert_equal mixed_case_email.downcase, @user.reload.email
+	end
+
+	test "email should be unique" do
+		user_2 = @user.dup
+		user_2.email = @user.email.upcase
+		@user.save
+		assert_not user_2.valid?
+	end
+
+	test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
 end
