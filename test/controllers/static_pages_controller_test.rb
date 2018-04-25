649c3decb989cb0f48720492a6a4fbf0a3a45e4d
David Michaux <michaux.dr@gmail.com>
2018-04-25 10:33:05 -0400

    Add tests for home and help routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@site_name = "Website Name"
+	end
+
+	test "should get home" do
+		get root_path
+		assert_response :success
+		assert_select "title", "#{@site_name}"
+	end
+
+	test "should get help" do
+		get help_path
+		assert_response :success
+		assert_select "title", "Help | #{@site_name}"
+	end
 end
