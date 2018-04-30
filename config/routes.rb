6afe0b9982c7dc805d8857a75b7c040d61cecc1b
David Michaux <michaux.dr@gmail.com>
2018-04-30 10:12:27 -0400

    Initialize basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
-	root 'static_pages#home'
-	get  '/help', to: 'static_pages#help'
+	root 	 'static_pages#home'
+	get  	 '/help',   to: 'static_pages#help'
+	get  	 '/login',  to: 'sessions#new'
+	post 	 '/login',  to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
 end
