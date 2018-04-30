01d8cf28a867ae8727594550f2953159eb45eac8
David Michaux <michaux.dr@gmail.com>
2018-04-30 10:59:05 -0400

    test for failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			
+		else
+			flash.now[:notice] = "Invalid login credentials"
+			render :new
+		end
 	end
 
 	def destroy
