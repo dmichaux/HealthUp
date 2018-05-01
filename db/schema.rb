0868c2d56b2f22d83eb03a9b0367a96d48907faa
David Michaux <michaux.dr@gmail.com>
2018-05-01 09:35:21 -0400

    Begin account activations functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430163718) do
+ActiveRecord::Schema.define(version: 20180501133236) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20180430163718) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
