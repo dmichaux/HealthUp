70e144d071929b34cb879e0e3ad476331bb4b046
David Michaux <michaux.dr@gmail.com>
2018-05-01 09:46:36 -0400

    Add create callback to initialize activation token and digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token
+	attr_accessor :remember_token, :activation_token
 
-	before_save :downcase_email
+	before_save   :downcase_email
+  before_create :create_activation_digest
 
 	validates :name,  presence: true, length: { within: 3..25 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -46,4 +47,9 @@ class User < ApplicationRecord
 	def downcase_email
 		self.email.downcase!
 	end
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
