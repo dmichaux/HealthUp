b508563b3528d76b95fd04646b9eb3e80fef6176
David Michaux <michaux.dr@gmail.com>
2018-05-01 15:46:17 -0400

    Test for (admin-only) user creation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user_admin = users(:adam)
+		@user 			= users(:beth)
+	end
+
+	test "must be admin to get new" do
+		log_in_as @user
+		get new_user_path
+		assert_response :redirect
+		log_in_as @user_admin
+		get new_user_path
+		assert_response :success
+	end
+
+	test "must be admin to get index" do
+		log_in_as @user
+		get users_path
+		assert_response :redirect
+		log_in_as @user_admin
+		get users_path
+		assert_response :success
+	end
 end
---
+++ test/integration/user_creation_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class UserCreationTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user_admin = users(:adam)
+		@user 			= users(:beth)
+	end
+
+	test "non-admin cannot create new user" do
+		log_in_as @user
+		get new_user_path
+		assert_response :redirect
+		assert_no_difference "User.count" do
+			post users_path params: { user: { name: "TestName",
+																				email: "test@example.com" } }
+		end
+	end
+
+	test "invalid user creation information" do
+		log_in_as @user_admin
+		get new_user_path
+		assert_template "users/new"
+		assert_no_difference "User.count" do
+			post users_path params: { user: { name: "",
+																			 email: "" } }
+		end
+	end
+
+	test "valid user creation" do
+		log_in_as @user_admin
+		get new_user_path
+		assert_difference "User.count", 1 do
+			post users_path params: { user: { name: "TestName",
+																				email: "test@example.com" } }
+		end
+	end
+end
