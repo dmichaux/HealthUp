9087859d719a3ee6c6dc549aead0db33c61aea1d
David Michaux <michaux.dr@gmail.com>
2018-05-01 12:26:17 -0400

    Add Users controller with RESTful actions/routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		# if @user.save
+		# 	UserMailer.activation_email(@user).deliver_now
+		# 	flash[:notice] = "Activation email sent to client"
+		# 	redirect_to current_user
+		# else
+		# 	flash.now[:notice] = "Invalid name or email"
+		# 	render :new
+		# end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
+end
