3b5ca760eed325f948cccdcdc67fa7c7a5abaaa2
David Michaux <michaux.dr@gmail.com>
2018-05-03 11:11:00 -0400

    Test and implement password reset mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: "Activate Your New Account"
   end
+
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Reset Your Password"
+  end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -10,4 +10,11 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.account_activation(user)
   end
 
+  # Preview this email at
+  # http://localhost:3000/rails/mailers/user_mailer/password_reset
+  def password_reset
+  	user = User.first
+  	user.pass_reset_token = User.new_token
+  	UserMailer.password_reset(user)
+  end
 end
