fe5976b457a2b7733525c50220c6a261b5fd04b3
David Michaux <michaux.dr@gmail.com>
2018-05-04 16:00:11 -0400

    Add messages member to users resource

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 	post 	 '/login',  to: 'sessions#create'
 	delete '/logout', to: 'sessions#destroy'
 
-	resources :users
+	resources :users do
+		member do
+			get :messages
+		end
+	end
 	resources :account_activations, only: [:edit]
 	resources :password_resets,		  only: [:new, :create, :edit, :update]
 end
