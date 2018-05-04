92a043f6ed41fc999f9f0335236788dec80db547
David Michaux <michaux.dr@gmail.com>
2018-05-04 10:50:05 -0400

    Test and validate Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,10 @@ class Message < ApplicationRecord
 
 	belongs_to :from_user, class_name: "User"
 	belongs_to :to_user, 	 class_name: "User"
+
+	default_scope -> { order(created_at: :desc) }
+
+	validates :to_user_id, presence: true
+	validates :subject, 	 presence: true, length: { within: 1..50 }
+	validates :body, 			 presence: true, length: { within: 1..3000 }
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  	@user_1  = users(:adam)
+  	@user_2  = users(:beth)
+  	@message = @user_1.sent_messages.build(to_user_id: @user_2.id,
+  																				 subject: 	 "Hello",
+  																				 body: 			 "Have a great day!")
+  end
+
+  test "should be valid" do
+  	assert @message.valid?
+  end
+
+  test "to_user_id should be present" do
+  	@message.to_user_id = nil
+  	assert_not @message.valid?
+  end
+
+  test "subject should be present" do
+  	@message.subject = "   "
+  	assert_not @message.valid?
+  end
+
+  test "subject should not exceed 50 characters" do
+  	@message.subject = "x" * 51
+  	assert_not @message.valid?
+  end
+
+  test "body should be present" do
+  	@message.body = "    "
+  	assert_not @message.valid?
+  end
+
+   test "body should not exceed 3000 characters" do
+  	@message.body = "x" * 3001
+  	assert_not @message.valid?
+  end
+
+  test "should be ordered with most recent first" do
+  	assert_equal messages(:most_recent), Message.first
+  end
 end
