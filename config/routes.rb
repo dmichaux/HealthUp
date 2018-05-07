c91c2421072be101e153becc5e3dbe239b50957a
David Michaux <michaux.dr@gmail.com>
2018-05-07 12:44:17 -0400

    Add messages create route

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
 	end
 	resources :account_activations, only: [:edit]
 	resources :password_resets,		  only: [:new, :create, :edit, :update]
+	resources :messages,						only: :create
 end
