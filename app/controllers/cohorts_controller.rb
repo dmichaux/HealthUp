f83888b9a0fa952ddfd42f55f9f46fb060caa96a
David Michaux <michaux.dr@gmail.com>
2018-05-08 15:57:01 -0400

    Add basic RESTful actions

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,2 +1,26 @@
 class CohortsController < ApplicationController
+
+	def index
+		@cohorts = Cohort.all
+	end
+
+	def show
+		@cohort = Cohort.find(params[:id])
+	end
+
+	def new
+		@cohort = Cohort.new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
