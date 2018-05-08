e96a177216201cd7c1128e508e9afaee110af239
David Michaux <michaux.dr@gmail.com>
2018-05-08 14:31:48 -0400

    Add/migrate Cohort model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507202654) do
+ActiveRecord::Schema.define(version: 20180508182755) do
+
+  create_table "cohorts", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.integer "from_user_id"
