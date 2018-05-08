aad9f791c1c98956af0b401c7cb9861bca7da629
David Michaux <michaux.dr@gmail.com>
2018-05-08 12:20:05 -0400

    Add create action for Outside Messages controller

--- app/controllers/outside_messages_controller.rb
+++ app/controllers/outside_messages_controller.rb
@@ -5,5 +5,20 @@ class OutsideMessagesController < ApplicationController
 	end
 
 	def create
+		to_admin_id = { to_admin_id: User.where(admin: true).first.id }
+		@outside_message = OutsideMessage.new(outside_message_params
+																				 .merge(to_admin_id))
+		if @outside_message.save
+			flash[:notice] = "Your message was sent"
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def outside_message_params
+		params.require(:outside_message).permit(:name, :email, :body)
 	end
 end
