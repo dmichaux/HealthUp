b303c7333d329ed6a7e0a7dcd9addee1ce57fca9
David Michaux <michaux.dr@gmail.com>
2018-05-08 16:16:02 -0400

    Add require_participant before_action

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -1,14 +1,14 @@
 class CohortsController < ApplicationController
 
 	before_action :require_login
-	before_action :require_admin, except: :show
+	before_action :require_admin, 			except: :show
+	before_action :require_participant, only: 	:show
 
 	def index
 		@cohorts = Cohort.all
 	end
 
 	def show
-		@cohort = Cohort.find(params[:id])
 	end
 
 	def new
@@ -25,5 +25,14 @@ class CohortsController < ApplicationController
 	end
 
 	def destroy
+	end
+
+	private
+
+	def require_participant
+		@cohort = Cohort.find(params[:id])
+		unless current_user.cohort == @cohort || current_user.admin?
+			redirect_to root_path
+		end
 	end
 end
