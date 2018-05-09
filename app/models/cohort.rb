f7133f4bda7d5f0cfa7ac1b3257823242cce321b
David Michaux <michaux.dr@gmail.com>
2018-05-09 16:15:46 -0400

    Add methods to return 'unspecified' if date is nil

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -4,4 +4,12 @@ class Cohort < ApplicationRecord
 
 	validates :name,				presence: true, length: { within: 3..25 }
 	validates :description, presence: true, length: { within: 3..500 }
+
+	def starts
+		start_date || "unspecified"
+	end
+
+	def ends
+		end_date || "unspecified"
+	end
 end
