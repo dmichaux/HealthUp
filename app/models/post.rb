fbb6f64633ac18352808e2da9224a065fb1a3897
David Michaux <michaux.dr@gmail.com>
2018-05-18 14:48:48 -0400

    Test/validate Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   
   belongs_to :cohort
   belongs_to :author, class_name: "User"
+
+  default_scope -> { order(created_at: :desc) }
+
+  validates :title, 		presence: true, length: { within: 2..50 }
+  validates :body, 			presence: true, length: { within: 5..3000 }
+  validates :cohort_id, presence: true
+  validates :author_id, presence: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	def setup
+		@user = users(:adam)
+		@cohort = cohorts(:one)
+		@post = @user.posts.build(title: "Test Post", body: "Lorem ipsum...",
+															cohort_id: @cohort.id)
+	end
+
+	test "should be valid" do
+		assert @post.valid?
+	end
+
+	test "title must be present" do
+		@post.title = "  "
+		assert_not @post.valid?
+	end
+
+	test "title must be within 2 and 50 characters" do
+		@post.title = "x"
+		assert_not @post.valid?
+		@post.title = "x" * 51
+		assert_not @post.valid?
+	end
+
+	test "body must be present" do
+		@post.body = "  "
+		assert_not @post.valid?
+	end
+
+	test "body must be within 5 and 3000 characters" do
+		@post.body = "x" * 4
+		assert_not @post.valid?
+		@post.body = "x" * 3001
+		assert_not @post.valid?
+	end
+
+	test "cohort_id should be present" do
+		@post.cohort_id = nil
+		assert_not @post.valid?
+	end
+
+	test "author_id should be present" do
+		@post.author_id = nil
+		assert_not @post.valid?
+	end
+
+	test "should be ordered with most recent first" do
+  	assert_equal posts(:most_recent), Post.first
+  end
 end
