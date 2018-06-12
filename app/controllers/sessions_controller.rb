0f5d7fdecce8c45ebb08b4fa1aa6cdefe6503cdb
David Michaux <michaux.dr@gmail.com>
2018-06-12 15:53:59 -0400

    Remove old create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,26 +18,6 @@ class SessionsController < ApplicationController
 		end
 	end
 
-	# def create
-	# 	user = User.find_by(email: params[:session][:email].downcase)
-	# 	if user && user.authenticate(params[:session][:password])
-	# 		if user.activated?
-	# 			log_in user
-	# 			params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-	# 			flash[:success] = "Login Successful"
-	# 			redirect_to user
-	# 		else
-	# 			note =  "Account must be activated."
-	# 			note += "Check your email for an activation link"
-	# 			flash[:info] = note
-	# 			redirect_to root_path
-	# 		end
-	# 	else
-	# 		flash.now[:danger] = "Invalid login credentials"
-	# 		render :new
-	# 	end
-	# end
-
 	def destroy
 		log_out if logged_in?
 		redirect_to root_path
