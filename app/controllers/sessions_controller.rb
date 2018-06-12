51267ebc6c4f923dbf87073b58b8d495c870ac36
David Michaux <michaux.dr@gmail.com>
2018-06-12 15:51:52 -0400

    bugfix: login attempt as inactive user no longer throws error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,23 +5,38 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
-			if user.activated?
+		if user && user.activated?
+			if user.authenticate(params[:session][:password])
 				log_in user
 				params[:session][:remember_me] == "1" ? remember(user) : forget(user)
 				flash[:success] = "Login Successful"
 				redirect_to user
-			else
-				note =  "Account must be activated."
-				note += "Check your email for an activation link"
-				flash[:info] = note
-				redirect_to root_path
 			end
 		else
-			flash.now[:danger] = "Invalid login credentials"
+			flash.now[:danger] = "Invalid or Inactive Account"
 			render :new
 		end
 	end
+
+	# def create
+	# 	user = User.find_by(email: params[:session][:email].downcase)
+	# 	if user && user.authenticate(params[:session][:password])
+	# 		if user.activated?
+	# 			log_in user
+	# 			params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+	# 			flash[:success] = "Login Successful"
+	# 			redirect_to user
+	# 		else
+	# 			note =  "Account must be activated."
+	# 			note += "Check your email for an activation link"
+	# 			flash[:info] = note
+	# 			redirect_to root_path
+	# 		end
+	# 	else
+	# 		flash.now[:danger] = "Invalid login credentials"
+	# 		render :new
+	# 	end
+	# end
 
 	def destroy
 		log_out if logged_in?
