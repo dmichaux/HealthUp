ea9873559f82e4d10f2d8fa427657bc2161509fa
David Michaux <michaux.dr@gmail.com>
2018-06-18 08:51:49 -0400

    Test/Validate UserGoal model

--- app/models/user_goal.rb
+++ app/models/user_goal.rb
@@ -1,4 +1,7 @@
 class UserGoal < ApplicationRecord
 
 	belongs_to :user
+
+	validates :body, 		presence: true, length: { maximum: 250 }
+	validates :user_id, presence: true
 end
--- test/models/user_goal_test.rb
+++ test/models/user_goal_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserGoalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  	user = users(:adam)
+  	@goal = user.user_goals.build(body: "Goal 1 ...")
+  end
+
+  test "should be valid" do
+  	assert @goal.valid?
+  end
+
+  test "body should be present" do
+  	@goal.body = "   "
+  	assert_not @goal.valid?
+  end
+
+  test "body should not exceed 250 characters" do
+  	@goal.body = "x" * 251
+  	assert_not @goal.valid?
+  end
+
+  test "user_id should be present" do
+  	@goal.user_id = nil
+  	assert_not @goal.valid?
+  end
 end
