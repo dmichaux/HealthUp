fe0ef2368ecd7f0b904ec76301e675c926973563
David Michaux <michaux.dr@gmail.com>
2018-06-19 13:29:24 -0400

    Test/Validate CohortGoal model

--- app/models/cohort_goal.rb
+++ app/models/cohort_goal.rb
@@ -1,4 +1,7 @@
 class CohortGoal < ApplicationRecord
 
 	belongs_to :cohort
+
+	validates :body, 			presence: true, length: { maximum: 250 }
+	validates :cohort_id, presence: true
 end
--- test/models/cohort_goal_test.rb
+++ test/models/cohort_goal_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class CohortGoalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  	cohort = cohorts(:one)
+  	@goal  = cohort.goals.build(body: "Goal 1 ...")
+  end
+
+  test "should be valid" do
+  	assert @goal.valid?
+  end
+
+  test "body should be present" do
+  	@goal.body = "   "
+  	assert_not @goal.valid?
+  end
+
+  test "body should not exceed 250 characters" do
+  	@goal.body = "x" * 251
+  	assert_not @goal.valid?
+  end
+
+  test "cohort_id should be present" do
+  	@goal.cohort_id = nil
+  	assert_not @goal.valid?
+  end
 end
