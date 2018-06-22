55fa754b2d826e1fc3d66ec879f9d48b316c2468
David Michaux <michaux.dr@gmail.com>
2018-06-22 12:39:07 -0400

    Use @user.new_messages_count method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		if current_user?(@user)
-			@new_messages  = Message.where(to_user_id: @user.id).where(opened: false).count
-			@new_messages += OutsideMessage.where(opened: false).count if @user.admin?
+			@user_goal 		 			= UserGoal.new
+			@new_message_count  = @user.new_message_count
 		else
 			@message = Message.new
 		end
