f45c5aff34529d0f8b941ce21e257fa31b3f64b5
David Michaux <michaux.dr@gmail.com>
2018-11-20 11:45:21 -0500

    Allow Heroku to serve assets

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module WebsiteFull
     # Includes authenticity token in remote forms
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
+    # Allow Heroku to serve assets
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
